f841ed33e023b9bca596d83642ff3aee332ea060
gcmoony <geo9114@gmail.com>
2024-10-16 03:31:07 +0900

    Moved component features

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import {createRoot} from 'react-dom/client'
 import Button from '@mui/material/Button';
 import Navbar from "./components/Navbar/Navbar.jsx"
+import GeneralTable from "./components/GeneralTable/GeneralTable.jsx";
 
 const rootNode = createRoot(document.getElementById("root"))
+
 rootNode.render(<>
   <Navbar/>
-  <h1>Hello from app</h1>
-  <Button variant="contained">MUI Button</Button>
+  <GeneralTable/>
 </>)
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,10 @@ import { ButtonGroup, Button } from "@mui/material"
 export default function Navbar() {
   return (
     <nav>
-      <ButtonGroup variant='contained' aria-label='Basic button group'>
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+      <div>
+        <h1>GotIt?</h1>
+        <p>Inventory Management</p>
+      </div>
     </nav>
   )
 }
